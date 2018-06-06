86e78abb64eb6db6b626b15a69e839b2335a936e
lanboys <lan_bing2013@163.com>
2018-06-06 16:23:15 +0800

    获取请求参数

---
+++ src/main/java/com/bing/lan/springboot/_03/ParamController.java
@@ -0,0 +1,22 @@
+package com.bing.lan.springboot._03;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author lanbing
+ */
+
+@RestController
+@RequestMapping("/param")
+public class ParamController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String param(@RequestParam(value = "id", required = true,defaultValue = "111") Integer myId) {
+        return "Id: " + myId;
+    }
+    
+}
